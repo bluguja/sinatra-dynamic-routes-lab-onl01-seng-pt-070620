4850010f535920040e3da2c58b31260d22fc314c
Bala Luguja <bluguja@gmail.com>
2020-10-22 20:37:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,14 +1,38 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
-  get '/:operation/:number1/:number2' do
-    @result = params[:num1].to_i * params[:num2].to_i
-    "#{@result}"
-    
+  get '/reversename/:name' do 
+    #binding.pry 
+    params["name"].reverse
+  end
 
+  get '/square/:number' do 
+    #binding.pry
+    variable= params["number"].to_i*params["number"].to_i
+    variable.to_s
+  end 
 
-   @operation = params[:operation]
+  get '/say/:number/:phrase' do
+    #binding.pry
+    answer = ''
+    variable_number = params["number"].to_i
+    variable_number.times do
+      answer += params[:phrase] 
+    end
+    answer
+  end
+
+   get '/say/:word1/:word2/:word3/:word4/:word5' do
+   #binding.pry
+  # params["word1"] + " " + params["word2"] + " " + params["word3"] + " " + params["word4"] + " " + params["word5"] + "."
+  "#{params["word1"]}" + " " + "#{params["word2"]}" + " " + "#{params["word3"]}" + " " + "#{params["word4"]}" + " " + "#{params["word5"]}" "."
+  end
+ 
+    get '/:operation/:number1/:number2' do
+      #binding.pry
+      # @result = params[:num1].to_i * params[:num2].to_i
+      # "#{@result}"
+        @operation = params[:operation]
         @num1 = params[:number1].to_i
         @num2 = params[:number2].to_i
         case @operation
@@ -21,6 +45,13 @@ class App < Sinatra::Base
          when "divide"
           "#{@num1 / @num2}"
         end 
+      end 
+    
+end
+
+  # get '/multiply/:num1/:num2' do 
+    #   variable = params[:num1].to_i * params[:num2].to_i
+    #  variable.to_i
       end 
 
 end
